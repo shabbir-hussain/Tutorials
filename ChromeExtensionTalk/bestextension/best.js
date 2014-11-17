e9879969356dd4debdc7e695444450a17d3d4e07
Shabbir Hussain <mohd.husn001@gmail.com>
2014-11-17 16:37:32 -0500

    zipped matlab workshop 2

---
+++ ChromeExtensionTalk/bestextension/best.js
@@ -0,0 +1,4 @@
+var x = document.getElementsByTagName('a');
+for( var i =0 ; i< x.length;i++){
+	x[i].href="http://img2.wikia.nocookie.net/__cb20140730180232/thefutureofeuropes/images/2/21/Doge-meme-original-6518.jpg";
+}
